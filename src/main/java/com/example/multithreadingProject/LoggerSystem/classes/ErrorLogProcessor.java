023e383fa30e86d2173266696910b199d8bb8af7
Your Name <you@example.com>
2025-01-18 15:04:19 +0530

    Logging system Chain of responsibility Design Pattern

---
+++ src/main/java/com/example/multithreadingProject/LoggerSystem/classes/ErrorLogProcessor.java
@@ -0,0 +1,17 @@
+package com.example.multithreadingProject.LoggerSystem.classes;
+
+public class ErrorLogProcessor extends LogProcessor{
+
+    public ErrorLogProcessor(LogProcessor nextLogProcessor) {
+        super(nextLogProcessor);
+    }
+
+    @Override
+    public void log(int logLevel, String message) {
+        if (logLevel == ERROR) {
+            System.out.println("ERROR: " + message);
+        } else {
+            super.log(logLevel, message);
+        }
+    }
+}
