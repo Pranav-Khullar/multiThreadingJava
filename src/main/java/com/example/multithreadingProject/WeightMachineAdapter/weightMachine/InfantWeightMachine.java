acc7fb9df4b7159ceb7223511dc70f1f394ad9b1
Your Name <you@example.com>
2025-01-19 16:14:16 +0530

    Adapter design pattern for weight machine

---
+++ src/main/java/com/example/multithreadingProject/WeightMachineAdapter/weightMachine/InfantWeightMachine.java
@@ -0,0 +1,8 @@
+package com.example.multithreadingProject.WeightMachineAdapter.weightMachine;
+
+public class InfantWeightMachine implements WeightMachine{
+    @Override
+    public Double getWeightInPounds() {
+        return 15.00;
+    }
+}
---
+++ src/main/java/com/example/multithreadingProject/WeightMachineAdapter/weightMachine/WeightMachine.java
@@ -0,0 +1,5 @@
+package com.example.multithreadingProject.WeightMachineAdapter.weightMachine;
+
+public interface WeightMachine {
+    Double getWeightInPounds();
+}
